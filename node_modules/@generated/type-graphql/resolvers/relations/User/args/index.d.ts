df925db22e1da1a0d67e22cfa32fe030d0a82ba9
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-02-04 01:07:57 -0800

    Revert "generated models and resolvers using typegraphql-prisma integration"
    This reverts commit 5b8e233209b6bec1c2281499b6ec9929123e8bdf.

--- node_modules/@generated/type-graphql/resolvers/relations/User/args/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { UserPostArgs } from "./UserPostArgs";
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,14 @@
-import "reflect-metadata"
 import express from 'express'
-import {ApolloServer} from 'apollo-server-express'
-import { buildSchema } from 'type-graphql'
-import { resolvers } from "@generated/type-graphql";
-import { PrismaClient } from "@prisma/client";
-
-
+//import {ApolloServer} from 'apollo-server-express'
+//import { buildSchema } from 'graphql'
 
 
 async function main() {
-    const schema = await buildSchema({
-      resolvers,
-      validate: false,
-    })
-    const prisma = new PrismaClient();
-    const apolloServer = new ApolloServer({ 
-      schema,
-      context: () => ({ prisma }), 
-    })
+    //const schema = await buildSchema({})
+    //const apolloServer = new ApolloServer({ schema })
     const app = express()
-    await apolloServer.start()
-    apolloServer.applyMiddleware({ app })
-    app.listen(4000, () => console.log('server started on localhost:4000'))
+    //apolloServer.applyMiddleware({ app })
+    app.listen(4000)
 }
 
 main()
